c0d23dac4821625f3878aa1df6fee923a7bef00d
Andrey Vasilev <kovorot0875@mail.ru>
2023-02-20 00:35:13 +0300

    Average cropped added

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,3 +1,5 @@
+import static java.lang.Double.valueOf;
+
 public class SalesManager {
     protected int[] sales;
 
@@ -13,5 +15,22 @@ public class SalesManager {
             }
         }
         return max;
+    }
+
+    public int averageCropped() {
+        int minMeaning = 0;
+        int maxMeaning = 0;
+        int sumMeaning = 0;
+        for (int sale : sales) {
+            if (minMeaning == 0 || minMeaning > sale) {
+                minMeaning = sale;
+            }
+            if (maxMeaning == 0 || maxMeaning < sale) {
+                maxMeaning = sale;
+            }
+            sumMeaning += sale;
+        }
+        sumMeaning = sumMeaning - (maxMeaning + minMeaning);
+        return (sumMeaning / (sales.length - 2));
     }
 }
